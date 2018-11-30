8fbaaf3c2e2fb0e6ebf753d425710c54dc241418
karthikeyankkn <connecttokkn@hotmail.com>
2018-11-30 13:00:56 +0530

    student index page done

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get 'students/index'
   root 'courses#index'
 
   get 'about', to: 'pages#about'
   get 'courses/new', to: 'courses#new'
+
+  resources :students, except: [:destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
